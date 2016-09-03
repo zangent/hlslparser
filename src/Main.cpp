40e901209cefab4e2879a199c6105c54976554c3
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2016-09-02 23:00:49 -0700

    Add HLSL generation to command line tool
    New command line arguments control the target language:
    
    -glsl - GLSL
    -hlsl - DX10-style HLSL
    -legacyhlsl - DX9-style HLSL

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,9 +1,23 @@
 #include "HLSLParser.h"
 #include "GLSLGenerator.h"
+#include "HLSLGenerator.h"
 
 #include <fstream>
 #include <sstream>
 #include <iostream>
+
+enum Target
+{
+    Target_VertexShader,
+    Target_FragmentShader,
+};
+
+enum Language
+{
+	Language_GLSL,
+	Language_HLSL,
+	Language_LegacyHLSL,
+};
 
 std::string ReadFile( const char* fileName )
 {
@@ -26,7 +40,10 @@ void PrintUsage()
 		<< "optional arguments:\n"
 		<< " -h, --help  show this help message and exit\n"
 		<< " -fs         generate fragment shader (default)\n"
-		<< " -vs         generate vertex shader\n";
+		<< " -vs         generate vertex shader\n"
+		<< " -glsl       generate GLSL (default)\n"
+		<< " -hlsl       generate HLSL\n"
+		<< " -legacyhlsl generate legacy HLSL\n";
 }
 
 int main( int argc, char* argv[] )
@@ -36,8 +53,9 @@ int main( int argc, char* argv[] )
 	// Parse arguments
 	const char* fileName = NULL;
 	const char* entryName = NULL;
-	GLSLGenerator::Target target = GLSLGenerator::Target_FragmentShader;
-	GLSLGenerator::Version version = GLSLGenerator::Version_140;
+
+	Target target = Target_FragmentShader;
+	Language language = Language_GLSL;
 
 	for( int argn = 1; argn < argc; ++argn )
 	{
@@ -50,11 +68,23 @@ int main( int argc, char* argv[] )
 		}
 		else if( String_Equal( arg, "-fs" ) )
 		{
-			target = GLSLGenerator::Target_FragmentShader;
+			target = Target_FragmentShader;
 		}
 		else if( String_Equal( arg, "-vs" ) )
 		{
-			target = GLSLGenerator::Target_VertexShader;
+			target = Target_VertexShader;
+		}
+		else if( String_Equal( arg, "-glsl" ) )
+		{
+			language = Language_GLSL;
+		}
+		else if( String_Equal( arg, "-hlsl" ) )
+		{
+			language = Language_HLSL;
+		}
+		else if( String_Equal( arg, "-legacyhlsl" ) )
+		{
+			language = Language_LegacyHLSL;
 		}
 		else if( fileName == NULL )
 		{
@@ -66,7 +96,7 @@ int main( int argc, char* argv[] )
 		}
 		else
 		{
-			Log_Error( "Too many arguments" );
+			Log_Error( "Too many arguments\n" );
 			PrintUsage();
 			return 1;
 		}
@@ -74,7 +104,7 @@ int main( int argc, char* argv[] )
 
 	if( fileName == NULL || entryName == NULL )
 	{
-		Log_Error( "Missing arguments" );
+		Log_Error( "Missing arguments\n" );
 		PrintUsage();
 		return 1;
 	}
@@ -88,14 +118,33 @@ int main( int argc, char* argv[] )
 	HLSLTree tree( &allocator );
 	if( !parser.Parse( &tree ) )
 	{
-		Log_Error( "Parsing failed, aborting" );
+		Log_Error( "Parsing failed, aborting\n" );
 		return 1;
 	}
 
 	// Generate output
-	GLSLGenerator generator( &allocator );
-	generator.Generate( &tree, target, version, entryName );
-	std::cout << generator.GetResult();
+	if (language == Language_GLSL)
+	{
+		GLSLGenerator generator( &allocator );
+		if (!generator.Generate( &tree, GLSLGenerator::Target(target), GLSLGenerator::Version_140, entryName ))
+		{
+			Log_Error( "Translation failed, aborting\n" );
+			return 1;
+		}
+
+		std::cout << generator.GetResult();
+	}
+	else
+	{
+		HLSLGenerator generator( &allocator );
+		if (!generator.Generate( &tree, HLSLGenerator::Target(target), entryName, language == Language_LegacyHLSL ))
+		{
+			Log_Error( "Translation failed, aborting\n" );
+			return 1;
+		}
+
+		std::cout << generator.GetResult();
+	}
 
 	return 0;
 }
